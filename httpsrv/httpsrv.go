7720cc8aae258cf81bbf4b9903576ebbab9853b3
Jon Lundy <jon.lundy@rakuten.com>
2018-03-16 08:31:06 -0600

    add graceful shutdown

--- httpsrv/httpsrv.go
+++ httpsrv/httpsrv.go
@@ -14,6 +14,9 @@ import (
 	"sort"
 	"sour.is/x/toolbox/ident"
 	"sour.is/x/toolbox/log"
+	"flag"
+	"os"
+	"os/signal"
 )
 
 // Example Usage
@@ -149,29 +152,48 @@ func Run() {
 
 	log.Notice("Listen and Serve on ", listen)
 
-	server = new(http.Server)
-	server.Addr = listen
-	server.Handler = router
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second * 15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
 
-	close(SignalStartup)
-	log.Notice(server.ListenAndServe())
-}
-
-func Shutdown() {
-	close(SignalShutdown)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
-	defer cancel()
-
-	go server.Shutdown(ctx)
-
-	select {
-	case <-ctx.Done():
-		log.Error(ctx.Err()) // prints "context deadline exceeded"
-		server.Close()
+	srv := &http.Server{
+		Addr:         listen,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler: router, // Pass our instance of gorilla/mux in.
 	}
 
-	WaitShutdown.Wait()
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error(err)
+		}
+	}()
+	close(SignalStartup)
+
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	close(SignalShutdown)
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Notice("shutting down")
 }
 
 func init() {
