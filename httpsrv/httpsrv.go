643f51057c5d1796c9a253bde88c27668d201a5f
Jon Lundy <me@sour.is>
2018-07-27 21:00:51 -0600

    Update httpsrv.go

--- httpsrv/httpsrv.go
+++ httpsrv/httpsrv.go
@@ -163,13 +163,6 @@ func Run() {
 		cfg := &tls.Config{
         		MinVersion:               tls.VersionTLS12,
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-			    tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			    tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			    tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			    tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
     		}
 		
 		srv.TLSConfig = cfg
