caf0f2d93a670c9d430ab31c09a4537bf5e6b001
Jon Lundy <jon.lundy@rakuten.com>
2020-05-28 16:59:25 -0600

    release version 0.12.13

--- dbm/dbinfo.go
+++ dbm/dbinfo.go
@@ -5,24 +5,25 @@ import (
 	"reflect"
 	"strings"
 	"unicode"
+
+	sq "github.com/Masterminds/squirrel"
 )
 
 // DbInfo database model metadata
 type DbInfo struct {
-	Table string
-	View  string
-	Cols  []string
-	SCols []string
-	GCols []string
-	index map[string]int
-	Auto  []string
-	ID    string
-	HasID bool
-	ROnly []string
+	Table     string
+	View      string
+	Cols      []string
+	SCols     []string
+	GCols     []string
+	index     map[string]int
+	Auto      []string
+	Primary   []string
+	Secondary []string
 }
 
 // SCol returns the struct column names
-func (d DbInfo) SCol(column string) (s string, err error) {
+func (d *DbInfo) SCol(column string) (s string, err error) {
 	idx, ok := d.Index(column)
 	if !ok {
 		err = fmt.Errorf("column not found on table: %v", column)
@@ -32,7 +33,7 @@ func (d DbInfo) SCol(column string) (s string, err error) {
 }
 
 // GCol returns the graphql column names
-func (d DbInfo) GCol(column string) (s string, err error) {
+func (d *DbInfo) GCol(column string) (s string, err error) {
 	idx, ok := d.Index(column)
 	if !ok {
 		err = fmt.Errorf("column not found on table: %v", column)
@@ -42,7 +43,7 @@ func (d DbInfo) GCol(column string) (s string, err error) {
 }
 
 // Col returns the mapped column names
-func (d DbInfo) Col(column string) (s string, err error) {
+func (d *DbInfo) Col(column string) (s string, err error) {
 	idx, ok := d.Index(column)
 	if !ok {
 		err = fmt.Errorf("column not found on table: %v", column)
@@ -52,7 +53,7 @@ func (d DbInfo) Col(column string) (s string, err error) {
 }
 
 // ColPanic returns the mapped column names will panic if col does not exist
-func (d DbInfo) ColPanic(column string) string {
+func (d *DbInfo) ColPanic(column string) string {
 	idx, ok := d.Index(column)
 	if !ok {
 		err := fmt.Errorf("column not found on table: %v", column)
@@ -62,13 +63,14 @@ func (d DbInfo) ColPanic(column string) string {
 }
 
 // Index returns the column number
-func (d DbInfo) Index(column string) (idx int, ok bool) {
+func (d *DbInfo) Index(column string) (idx int, ok bool) {
 	idx, ok = d.index[column]
 	return
 }
 
 // GetDbInfo builds a metadata struct
-func GetDbInfo(o interface{}) (d DbInfo) {
+func GetDbInfo(o interface{}) *DbInfo {
+	d := &DbInfo{}
 	t := reflect.TypeOf(o)
 
 	d.Table = t.Name()
@@ -85,22 +87,19 @@ func GetDbInfo(o interface{}) (d DbInfo) {
 			d.View = view
 		}
 
-		dbField := field.Tag.Get("db")
-		sp := append(strings.SplitN(dbField, ",", 2), "")
+		sp := append(strings.SplitN(field.Tag.Get("db"), ",", 2), "")
 
 		tag := sp[0]
 
-		if strings.Contains(dbField, ",AUTO") {
-			d.Auto = append(d.Auto, field.Name)
-		}
-
-		if strings.Contains(dbField, ",RO") {
-			d.ROnly = append(d.ROnly, field.Name)
-		}
-
-		if strings.Contains(dbField, ",ID") {
-			d.HasID = true
-			d.ID = field.Name
+		for _, opt := range sp[1:] {
+			switch strings.ToUpper(opt) {
+			case "AUTO":
+				d.Auto = append(d.Auto, field.Name)
+			case "PRIMARY":
+				d.Primary = append(d.Primary, field.Name)
+			case "SECONDARY":
+				d.Secondary = append(d.Secondary, field.Name)
+			}
 		}
 
 		json := field.Tag.Get("json")
@@ -154,7 +153,7 @@ func GetDbInfo(o interface{}) (d DbInfo) {
 }
 
 // StructMap accepts a struct and the columns to be set and returns a []interface{} that can be passed to a query scan
-func (d DbInfo) StructMap(o interface{}, cols []string) (fields []string, targets []interface{}, err error) {
+func (d *DbInfo) StructMap(o interface{}, cols []string) (fields []string, targets []interface{}, err error) {
 	if cols == nil {
 		cols = d.SCols
 	}
@@ -190,4 +189,48 @@ func (d DbInfo) StructMap(o interface{}, cols []string) (fields []string, target
 	}
 
 	return
+}
+
+func (d *DbInfo) PrimaryEq(args ...interface{}) (sq.Eq, error) {
+	if len(args) != len(d.Primary) {
+		return nil, fmt.Errorf("argument missmatch. want=%d, got=%d", len(args), len(d.Primary))
+	}
+
+	q := sq.Eq{}
+	for i, k := range d.Primary {
+		q[k] = args[i]
+	}
+
+	return q, nil
+}
+
+func (d *DbInfo) PrimaryPanicEq(args ...interface{}) sq.Eq {
+	q, err := d.PrimaryEq(args...)
+	if err != nil {
+		panic(err)
+	}
+
+	return q
+}
+
+func (d *DbInfo) SecondaryEq(args ...interface{}) (sq.Eq, error) {
+	if len(args) != len(d.Secondary) {
+		return nil, fmt.Errorf("argument missmatch. want=%d, got=%d", len(args), len(d.Secondary))
+	}
+
+	q := sq.Eq{}
+	for i, k := range d.Secondary {
+		q[k] = args[i]
+	}
+
+	return q, nil
+}
+
+func (d *DbInfo) SecondaryPanicEq(args ...interface{}) sq.Eq {
+	q, err := d.SecondaryEq(args...)
+	if err != nil {
+		panic(err)
+	}
+
+	return q
 }
